e2095b5f00689e61699f681ab01c408f61ffb4dd
AIWASS23 <marcelodearaujo677@gmail.com>
2022-12-27 08:48:09 -0300

    onDelete in SwiftUI

--- SwiftUI/Propriedades/ContentView.swift
+++ SwiftUI/Propriedades/ContentView.swift
@@ -33,6 +33,23 @@ struct ContentView: View {
                         Text(item.amount, format: .currency(code: "USD"))
                     }
                 }
+                /*
+                    O onDelete é um modificador que pode ser usado em elementos de lista no SwiftUI 
+                    para especificar uma ação a ser executada quando o usuário desliza um item para a 
+                    esquerda e toca no botão de exclusão. Ele é geralmente usado em conjunto com a propriedade
+                    .editing para permitir que o usuário exclua itens da lista.
+
+                    O onDelete(perform:) é uma variante do modificador onDelete que permite 
+                    especificar uma ação a ser executada quando o usuário desliza um item para a 
+                    esquerda e toca no botão de exclusão de uma lista. 
+
+                    A diferença entre o onDelete e o onDelete(perform:) é que o onDelete(perform:) 
+                    permite que você especifique uma ação externa a ser executada quando o usuário 
+                    exclui um item, enquanto o onDelete permite que você coloque o código de 
+                    exclusão diretamente na lista. 
+                    Isso pode ser útil em situações em que o código de exclusão é 
+                    complexo ou se você deseja reutilizar o código em várias listas.
+                */
                 .onDelete(perform: removeItems)
             }
             .navigationTitle("")
