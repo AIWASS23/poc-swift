e13d407d5220fcdfc1473c7cbb94caa3da2dad9a
AIWASS23 <marcelodearaujo677@gmail.com>
2023-01-13 22:20:05 -0300

    Estudo sobre onAppear e onChange

--- SwiftUI/TabView/ImageSaver.swift
+++ SwiftUI/TabView/ImageSaver.swift
@@ -1,5 +1,16 @@
 import UIKit
 
+/*
+    A implementação do ImageSaver é usada para salvar uma imagem em álbum de fotos do dispositivo. A classe 
+    contém dois handlers, successHandler e errorHandler, que são chamados dependendo do resultado da 
+    operação de salvar a imagem. A classe contém uma função writeToPhotoAlbum que recebe uma imagem do tipo 
+    UIImage e usa a função UIImageWriteToSavedPhotosAlbum para salvar a imagem no álbum de fotos. 
+    Esta função tem como segundo parâmetro o próprio objeto da classe, o terceiro parâmetro é a função 
+    que será chamada quando a operação for completada. A função saveCompleted é chamada quando a operação 
+    de salvar a imagem for completada, essa função verifica se houve algum erro ao salvar a imagem, 
+    se sim, chama o errorHandler, se não, chama o successHandler.
+*/
+
 class ImageSaver: NSObject {
     var successHandler: (() -> Void)?
     var errorHandler: ((Error) -> Void)?
