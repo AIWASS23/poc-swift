93354c60c3bd32568854b9d5fb9becd5fedfb99f
AIWASS23 <marcelodearaujo677@gmail.com>
2023-08-31 08:30:33 -0300

    Estudo sobre Sons e Mascaras

---
+++ Sound/SoundEffects/ContentView.swift
@@ -0,0 +1,20 @@
+import SwiftUI
+
+struct ContentView: View {
+    
+    var body: some View {
+        VStack(spacing: 10) {
+            ForEach(SoundOptions.allCases,id: \.self){ option in
+                SoundView(options: option) {
+                    SoundManager.instance.playSound(sound: option)
+                }
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Sound/SoundEffects/SoundManager.swift
@@ -0,0 +1,26 @@
+import Foundation
+import AVKit
+
+class SoundManager{
+    static let instance = SoundManager()
+    
+    var player:AVAudioPlayer?
+    
+    func playSound(sound:SoundOptions){
+        guard let url = Bundle.main.url(forResource: sound.rawValue, withExtension: ".mp3") else {return}
+        do{
+            player = try AVAudioPlayer(contentsOf: url)
+            player?.play()
+        }catch let error{
+            print("Error: \(error.localizedDescription)")
+        }
+        
+    }
+}
+
+enum SoundOptions:String,CaseIterable{
+    case ship
+    case duck
+    case heart
+    case cow
+}
