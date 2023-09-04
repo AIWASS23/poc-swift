5ebaa2dd0fe87acc334570d070f6285a2361396a
AIWASS23 <marcelodearaujo677@gmail.com>
2023-09-04 16:29:04 -0300

    Estudo sobre Integraçao de dados

---
+++ EscapingClosure/DownloadModel.swift
@@ -0,0 +1,5 @@
+import Foundation
+
+struct DownloadModel {
+    let data:String
+}
---
+++ Typealias/MovieModel.swift
@@ -0,0 +1,7 @@
+import Foundation
+
+struct MovieModel {
+    let title:String
+    let director:String
+    let count:Int
+}
