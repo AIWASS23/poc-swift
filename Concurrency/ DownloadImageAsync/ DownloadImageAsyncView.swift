c8648520ac1e5a65b705305e926430f408d02976
AIWASS23 <marcelodearaujo677@gmail.com>
2023-09-08 17:19:45 -0300

    Estudo de Concurrency no swift

---
+++ Concurrency/ DownloadImageAsync/ DownloadImageAsyncView.swift
@@ -0,0 +1,36 @@
+import Foundation
+import SwiftUI
+
+struct DownloadImageAsyncView: View{
+    
+    @StateObject private var viewModel = DownloadImageAsyncViewModel()
+    
+    var body: some View{
+        ZStack{
+            Color.black.ignoresSafeArea()
+            if let image = viewModel.image{
+                Image(uiImage: image)
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: 250,height: 250)
+                    .cornerRadius(20)
+                    .onTapGesture {
+                        viewModel.fetImagesWithTask()
+                    }
+            }
+        }
+        .onAppear{
+            // 1
+            //viewModel.fetchImagesWithComplpetionClousure()
+            
+            // 2
+            //viewModel.fetImagesWithCombine()
+            
+            // 3
+            //viewModel.fetImagesWithCombine2()
+            
+            // 4
+            viewModel.fetImagesWithTask()
+        }
+    }
+}
---
+++ Concurrency/SwiftConcurrencyApp.swift
@@ -0,0 +1,36 @@
+import SwiftUI
+
+@main
+struct SwiftConcurrencyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            // 1
+            //DoCatchTryThrowView(manager: DoCatchTryThrowDataManager())
+            
+            // 2
+            //DownloadImageAsyncView()
+            
+            // 3
+            //AsyncAwaitView()
+            
+            // 4
+            //TaskHomeView()
+            
+            // 5
+            //AsyncLetView()
+            
+            // 6
+            //TaskGroupView(manager: TaskGroupDataManager(), urlString: "https://picsum.photos/200")
+            
+            // 7
+            //CheckedContinuationView(dataService: CheckedContinuationDataServices(), urlString: "https://picsum.photos/200")
+            
+            // 8
+           // StructClassActor()
+            //StructClassActorHomeView()
+            
+            // 9
+            ActorsView()
+        }
+    }
+}
