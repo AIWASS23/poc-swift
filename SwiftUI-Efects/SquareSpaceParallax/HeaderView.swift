13a3f0598f62f2476fd1f06b2e7ee3719963c8de
AIWASS23 <marcelodearaujo677@gmail.com>
2024-01-23 09:47:33 -0300

    :sparkles: feat: estudo diario de swift

---
+++ SwiftUI-Efects/SquareSpaceParallax/HeaderView.swift
@@ -0,0 +1,32 @@
+import SwiftUI
+
+struct HeaderView: View {
+    var body: some View {
+        // headerView
+        HStack {
+            Button(action: {}, label: {
+                Image(systemName: "magnifyingglass")
+                    .font(.title2)
+            })
+            
+            Spacer()
+            
+            Button(action: {}, label: {
+                Image(systemName: "line.horizontal.3")
+                    .font(.title2)
+            })
+        }
+        .padding()
+        .overlay(
+            // Title
+            Text("SquareSpace")
+                .font(.title2)
+                .fontWeight(.bold)
+        )
+        .padding(.top, UIApplication.shared.windows.first?.safeAreaInsets.top)
+        .background(Color("Color"))
+        .foregroundColor(.white)
+        // Shadow
+        .shadow(color: Color.black.opacity(0.15), radius: 5, x: 0, y: 5)
+    }
+}
