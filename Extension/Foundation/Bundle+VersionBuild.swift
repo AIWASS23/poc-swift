bbd8bd8240f9e505015e4c8c38161c8d8a6f5d87
AIWASS23 <marcelodearaujo677@gmail.com>
2025-01-05 15:40:34 -0300

    alualizando estudos de domingo

--- Extension/Foundation/Bundle+VersionBuild.swift
+++ Extension/Foundation/Bundle+VersionBuild.swift
@@ -23,4 +23,23 @@ extension Bundle {
     var fullVersion: String {
         return "\(shortVersion) (\(buildVersion))"
     }
+
+    func decode<T: Codable>(_ file: String) -> T {
+
+        guard let url = self.url(forResource: file, withExtension: nil) else {
+            fatalError("Failed to locate \(file) in bundle.")
+        }
+    
+        guard let data = try? Data(contentsOf: url) else {
+            fatalError("Failed to load \(file) from bundle.")
+        }
+    
+        let decoder = JSONDecoder()
+    
+        guard let loaded = try? decoder.decode(T.self, from: data) else {
+        fatalError("Failed to decode \(file) from bundle.")
+        }
+    
+        return loaded
+    }
 }
